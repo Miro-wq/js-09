d5cafd4493277346ed5103eed02e33f85c37446d
Miro <Miro-wq@users.noreply.github.com>
2024-07-08 23:23:23 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,116 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+let delayId = null;
+let stepId = null;
+let amountId = null;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const submitHandler = e => {
+  e.preventDefault();
+  if (!e.target.tagName === 'BUTTON') return;
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  delayId = Number(delay.value);
+  stepId = Number(step.value);
+  amountId = Number(amount.value);
+
+  for (let i = 1; i <= amountId; i++) {
+    createPromise(i, delayId)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayId += stepId;
+  }
+
+  e.currentTarget.reset();
+};
+formEl.addEventListener('submit', submitHandler);
+
+
+
+// =========================DRAFT======================================
+
+//import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo");
+//   }, 300);
+// });
+
+
+// let myPromise = new Promise(function(myResolve, myReject) {
+//   // "Producing Code" (May take some time)
+  
+//     myResolve(); // when successful
+//     myReject();  // when error
+//   });
+  
+//   // "Consuming Code" (Must wait for a fulfilled Promise)
+//   myPromise.then(
+//     function(value) { /* code if successful */ },
+//     function(error) { /* code if some error */ }
+//   );
+
+// function myDisplayer(some) {
+//   document.getElementById("demo").innerHTML = some;
+// }
+
+// let myPromise = new Promise(function(myResolve, myReject) {
+//   let x = 0;
+
+// // The producing code (this may take some time)
+
+//   if (x == 0) {
+//     myResolve("OK");
+//   } else {
+//     myReject("Error");
+//   }
+// });
+
+// myPromise.then(
+//   function(value) {myDisplayer(value);},
+//   function(error) {myDisplayer(error);}
+// );
+
+// When	Call
+// Success	myResolve(result value)
+// Error	myReject(error object)
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
